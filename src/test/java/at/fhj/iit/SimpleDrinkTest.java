fee389a3d6541fe0deb4cc3169adb9d2eb163d6b
RegalFenster <48275164+regalfenster@users.noreply.github.com>
2020-05-06 11:14:03 +0200

    Implement SimpleDrinkTest setup

--- src/test/java/at/fhj/iit/SimpleDrinkTest.java
+++ src/test/java/at/fhj/iit/SimpleDrinkTest.java
@@ -1,4 +1,23 @@
 package at.fhj.iit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+@DisplayName("Testing SimpleDrink implementation")
 public class SimpleDrinkTest {
+    private SimpleDrink soda;
+    private Liquid water;
+
+    @BeforeEach
+    void setup() {
+        // SETUP PHASE
+        water = new Liquid("Wasser", 0.3, 0);
+        soda = new SimpleDrink("Soda", water);
+    }
+
 }
